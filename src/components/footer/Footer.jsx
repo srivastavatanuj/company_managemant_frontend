c382f58c14136b00fe659a0a03e6f73d3ae813c4
jugendragangwar <jugendragangwar8791@gmail.com>
2024-08-27 16:02:36 +0530

    tailwind configure

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,3 +1,12 @@
-const Footer = () => {};
+const Footer = () => {
+    return(
+        <div>
+           <footer className="bg-red-800">
+            <h1 className="text-red-300">Footer</h1>
+           </footer>
+        </div>
+    )
+};
+
 
 export default Footer;
