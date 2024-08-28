31276fb596127863c4cef6f42f7a2b945e66d1e4
Riteshkaloni <ritesh.kaloni1995@gmail.com>
2024-08-28 10:35:46 +0530

    some changes done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,9 +9,9 @@ const Header = () => {
       <div className=" container mx-auto flex justify-between items-center">
       <img className='h-10 w-1/6' src={img} alt="Kurmato Logo" />
         <div className="text-black text-lg flex justify-center">
-        <a href="#" className="px-3 py-2 hover:bg-gray-700 rounded">Services</a>
-          <a href="#" className="px-3 py-2 hover:bg-gray-700 rounded">About</a>
-          <a href="#" className="px-3 py-2 hover:bg-gray-700 rounded">Services</a>
+        <a href="#" className="px-3 py-2 font-bold ">Home</a>
+          <a href="#" className="px-3 py-2 rounded font-bold ">Employee</a>
+          <a href="#" className="px-3 py-2 font-bold">Project</a>
         </div>
         <div>
           <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
