811e24880530c4703b3fe2ebc5c722739863218a
Riteshkaloni <ritesh.kaloni1995@gmail.com>
2024-08-29 17:26:16 +0530

    resolve all conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,31 @@
-<<<<<<< HEAD
-import Header from "./components/header/Header.jsx";
-import ProjectDetails from "./components/Project_details/ProjectDetails.jsx";
-import Contact from "./pages/ContactUs/contact.jsx";
-import Project from "./pages/project/Project.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import ContactUs from "./pages/ContactUs";
+import EmployeeList from "./pages/Employee/EmployeeList";
+import EmployeeDetail from "./pages/Employee/EmployeeDetail";
+import LoginForm from "./pages/Login/LoginForm";
+import Header from "./components/header/Header";
+import Project from "./pages/project/Project.jsx";
+import ProjectDetails from "./pages/project/ProjectDetails.jsx";
 import Data from "./assets/data.js";
-
-function App() {
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Header />}></Route>
-        <Route
-          path="/project" element={<Project projects={Data} />}
-        ></Route>
-        <Route path="/contactus" element={<Contact />}></Route>
-      <Route path="/project/:id" element={<ProjectDetails/>}></Route>
-
-=======
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home/Home';
-import ContactUs from './pages/ContactUs';
-import EmployeeList from './pages/Employee/EmployeeList'; 
-import EmployeeDetail from './pages/Employee/EmployeeDetail';  
-import LoginForm from './pages/Login/LoginForm';
-import Header from './components/header/Header';
+import Footer from './components/footer/Footer.jsx'
 
 
 function App() {
   return (
     <BrowserRouter>
-     <Header/>
+      <Header />
       <Routes>
-        <Route path='/login' element={<LoginForm/>}/>
-        <Route path='/home' element={<Home />} />
-        <Route path='/contact' element={<ContactUs />} />
-        <Route path='/employee' element={<EmployeeList />} /> 
-        <Route path='/employee/:id' element={<EmployeeDetail />} /> 
->>>>>>> 9be6bb0fa0a069c9e015557bf66868724126574c
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/project" element={<Project projects={Data} />}></Route>
+        <Route path="/project/:id" element={<ProjectDetails />}></Route>
+        <Route path="/home" element={<Home />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/employee" element={<EmployeeList />} />
+        <Route path="/employee/:id" element={<EmployeeDetail />} />
       </Routes>
+    <Footer/>
+
     </BrowserRouter>
   );
 }
